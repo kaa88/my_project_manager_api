7a8b808aabcd3efefa1ada115c96328989e14972
Andrei <dre021088@gmail.com>
2024-08-20 11:49:54 +0700

    update: move middleware to router

--- src/shared/routers/basicRouter.js
+++ src/shared/routers/basicRouter.js
@@ -3,6 +3,18 @@ import { ApiError } from "../../services/error/apiError.js";
 import { Message } from "../../services/error/message.js";
 import { isArray, isObjectEmpty } from "../utils.js";
 
+import nullValueMiddleware from "../../services/nullValueMiddleware.js";
+import queryParserMiddleware from "../../services/queryParserMiddleware.js";
+import authMiddleware from "../../services/auth/middleware.js";
+import userRoleMiddleware from "../../services/userRoles/middleware.js";
+
+const middlewares = [
+  authMiddleware,
+  userRoleMiddleware,
+  nullValueMiddleware,
+  queryParserMiddleware,
+];
+
 export function BasicRouter({ controller, omit = [] }) {
   if (isObjectEmpty(controller))
     throw ApiError.internal("No controller provided");
@@ -12,22 +24,22 @@ export function BasicRouter({ controller, omit = [] }) {
   const router = express.Router();
 
   if (!omit.includes("create") && controller.create)
-    router.post("/", controller.create);
+    router.post("/", ...middlewares, controller.create);
 
   if (!omit.includes("update") && controller.update)
-    router.patch("/:id", controller.update);
+    router.patch("/:id", ...middlewares, controller.update);
 
   if (!omit.includes("delete") && controller.delete)
-    router.delete("/:id", controller.delete);
+    router.delete("/:id", ...middlewares, controller.delete);
 
   if (!omit.includes("findOne") && controller.findOne)
-    router.get("/:id", controller.findOne);
+    router.get("/:id", ...middlewares, controller.findOne);
 
   if (!omit.includes("findMany") && controller.findMany)
-    router.get("/", controller.findMany);
+    router.get("/", ...middlewares, controller.findMany);
 
   // if (!omit.includes("search") && controller.search)
-  //   router.get("/search", controller.search);
+  //   router.get("/search", ...middlewares, controller.search);
 
   return router;
 }
